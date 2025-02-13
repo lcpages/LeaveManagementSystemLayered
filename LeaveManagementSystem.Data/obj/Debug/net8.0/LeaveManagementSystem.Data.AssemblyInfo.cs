9772288bbb8b5b299e04aeee091a74e147ae3c29
Louis Cesar Pages <louis.pages@hexagon.com>
2025-02-13 15:21:33 +0100

    Complete Advanced UI module

--- LeaveManagementSystem.Data/obj/Debug/net8.0/LeaveManagementSystem.Data.AssemblyInfo.cs
+++ LeaveManagementSystem.Data/obj/Debug/net8.0/LeaveManagementSystem.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c6cb780510b63bfd2e221bf5a4f03c464fef43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a4a1bddbcc3abcb0990e9682a90327485e95f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
